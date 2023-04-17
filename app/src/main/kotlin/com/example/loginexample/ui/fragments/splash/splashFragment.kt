fbdc32347249924eeec250d738262375e5305500
Vladimir Grinkevich <84560225+VladimirGrinkevich@users.noreply.github.com>
2023-04-17 14:50:37 +1200

    Update splashFragment.kt

--- app/src/main/kotlin/com/example/loginexample/ui/fragments/splash/splashFragment.kt
+++ app/src/main/kotlin/com/example/loginexample/ui/fragments/splash/splashFragment.kt
@@ -14,7 +14,7 @@ import com.example.loginexample.ui.fragments.login.AuthViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class splashFragment : Fragment() {
+class splashFragment : Fragment() {                                     // The name of the class "splashFragment" should be written in pascal case style
 
     private lateinit var binding: FragmentSplashBinding
     private val viewModel by viewModels<AuthViewModel>()
